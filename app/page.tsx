9af84b925a233f0a96cf4db2bffcc197c78ce6d3
priest-2105 <priest2105@gmail.com>
2025-01-11 06:39:06 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <div className="">
       <h1 className="text-4xl">Welcome to Cardinal Eschool</h1>
-      {/* <Image src="/images/logo.png" width="200" height="200" alt="Cardinal Eschool" /> */}
-     
+      
     </div>
   );
 }
