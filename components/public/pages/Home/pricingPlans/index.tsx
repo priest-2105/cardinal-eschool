c033cfb150a29bfd7bd48cf0fdf7529bf88fcd8c
priest-2105 <priest2105@gmail.com>
2025-01-15 23:07:37 +0100

    pricing plan update

--- components/public/pages/Home/pricingPlans/index.tsx
+++ components/public/pages/Home/pricingPlans/index.tsx
@@ -63,8 +63,19 @@ const PricingPlans: React.FC = () => {
     },
   ];
 
+  const pricingDiscounts = [
+    {
+      title: "SAVE 10%",
+      duration: "When you Subscribe with us for 3-6 months",
+    },
+    {
+      title: "SAVE 10%",
+      duration: "When you refer your friends and family (Existing Clients Only)",
+    },
+  ];
+
   const cardVariants = {
-    hidden: { opacity: 0, y: 50 }, // Start hidden and slide up
+    hidden: { opacity: 0, y: 50 },  
     visible: (i: number) => ({
       opacity: 1,
       y: 0,
@@ -78,7 +89,7 @@ const PricingPlans: React.FC = () => {
   };
 
   return (
-    <div className="bg-[#fff] py-12 h-fit">
+    <div className="bg-[#E9FFFF] py-12 h-fit">
       {/* Header Section */}
       <div className="bg-[#1BC2C2] py-16 px-4 sm:px-8 lg:min-h-96 xlg:min-h-96 md:min-h-fit sm:min-h-fit lg:px-16">
         <h1 className="text-5xl sm:text-4xl font-bold text-center text-white mb-4">
@@ -123,31 +134,26 @@ const PricingPlans: React.FC = () => {
       </div>
 
 
-      <div className="lg:-mt-48 h-fit py-12 px-5">
-        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4 max-w-7xl mx-auto">
-          {pricingPlans.map((plan, index) => (
+      <div className="pt-16 h-fit ms-auto me-auto py-12 px-5">
+        <div className="flex  justify-center justify-self-center gap-8 max-w-7xl max-sm:block mx-auto">
+          {pricingDiscounts.map((plan, index) => (
             <motion.div
               drag="y"
               dragConstraints={{ top: -10, bottom: 10 }}
               key={index}
-              custom={index} // Pass the index to the variant
+              custom={index} 
               variants={cardVariants}
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: false, amount: 0.3 }} // Repeat animation on scroll
-              className="bg-white group rounded-3xl shadow-lg min-w-[250px] p-6 border-2 hover:border-white-2 hover:bg-[#1BC2C2] hover:text-white transition"
+              viewport={{ once: false, amount: 0.3 } }
+              className="bg-transparent items-center max-w-72 w-72  text-center text-[#1BC2C2] group rounded-3xl shadow-lg min-w-[250px] p-6 border-2 border-[#1BC2C2] hover:border-white-2 hover:bg-[#1BC2C2] hover:text-[#fff] transition"
             >
-              <h2 className="text-xl font-bold mb-4">{plan.title}</h2>
+              <h2 className="text-3xl py-2 font-bold mb-4">{plan.title}</h2>
               <p className="text-3xl font-bold text-[#1BC2C2] group-hover:text-white mb-2">
-                {plan.price}
+                {/* {plan.price} */}
               </p>
-              <p className="text-sm mb-4 font-bold">{plan.duration}</p>
-              <ul className="text-sm list-disk space-y-2 mb-6 font-semibold">
-                {plan.features.map((feature, i) => (
-                  <li key={i}>{feature}</li>
-                ))}
-              </ul>
-              <button className="bg-[#1BC2C2] text-white py-2 px-4 rounded-lg group-hover:bg-white group-hover:text-[#1BC2C2] w-full">
+              <p className="text-sm py-2 mb-4 font-bold">{plan.duration}</p>           
+            <button className="bg-[#1BC2C2] text-white py-2 px-4 rounded-lg group-hover:bg-white group-hover:text-[#1BC2C2] w-full">
                 Get Started
               </button>
             </motion.div>
