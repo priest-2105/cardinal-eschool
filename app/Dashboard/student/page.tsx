82e4613c69f3906492583d26228665bd8ea0d4dd
priest-2105 <priest2105@gmail.com>
2025-01-16 21:21:37 +0100

    payment

---
+++ app/Dashboard/student/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function StudentDashboard() {
+  return (
+    <div className="">
+      <h1 className="text-4xl">Welcome to Cardinal Eschool student dashboard</h1>
+      
+    </div>
+  );
+}
