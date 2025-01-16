e88c0841cc55a68eb17a1b1ce9d6d78208ecd822
priest-2105 <priest2105@gmail.com>
2025-01-16 22:54:57 +0100

    student dahsboard

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -81,7 +81,7 @@ const upcomingClasses = [
   },
 ]
 
-export function StudentDashboard() {
+export default function StudentDashboard() {
   return (
     <div className="space-y-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
