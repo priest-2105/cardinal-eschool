5f9f28ad3a632aed2632b026aa5612e9c26304c4
priest-2105 <priest2105@gmail.com>
2025-01-19 13:35:42 +0100

    dashboard layout fixed

--- components/dashboard/student/header/index.tsx
+++ components/dashboard/student/header/index.tsx
@@ -42,7 +42,7 @@ const Dropdown: React.FC<{ items: Notification[] | ProfileOption[], icon: React.
         {icon}
       </button>
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg">
+        <div className="absolute left-0 mt-2 w-48 z-25 bg-white border border-gray-200 rounded-lg shadow-lg">
           <ul className="py-1">
             {items.map((item, index) => (
               <li key={index} className="px-4 py-2 hover:bg-gray-100">
