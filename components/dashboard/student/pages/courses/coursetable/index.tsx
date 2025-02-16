8b735c7f0dcd346f233cee8563421a42a9586f02
priest-2105 <priest2105@gmail.com>
2025-02-16 07:51:28 +0100

    course list table

--- components/dashboard/student/pages/courses/coursetable/index.tsx
+++ components/dashboard/student/pages/courses/coursetable/index.tsx
@@ -9,15 +9,14 @@ interface CourseTableProps {
 export function CourseTable({ courses }: CourseTableProps) {
   return (
     <div className="relative h-[calc(90vh-200px)] scroll-smooth custom-scrollbar">
-      {" "} 
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead>Class</TableHead>
-            <TableHead>Tutor Assigned</TableHead>
-            <TableHead>Schedule</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Date Added</TableHead>
+            <TableHead className="w-1/3 md:w-1/4">Class</TableHead>
+            <TableHead className="hidden md:table-cell">Tutor Assigned</TableHead>
+            <TableHead className="hidden lg:table-cell">Schedule</TableHead>
+            <TableHead className="w-1/3 md:w-1/4">Status</TableHead>
+            <TableHead className="hidden xl:table-cell">Date Added</TableHead>
             <TableHead className="w-[50px]"></TableHead>
           </TableRow>
         </TableHeader>
