545cf359ac3e76617bb736e50e2e4ce0fc4b15e6
priest-2105 <priest2105@gmail.com>
2025-03-03 06:19:11 +0100

    admin announcement

--- app/(dashboard)/admin/(announcement)/announcements/page.tsx
+++ app/(dashboard)/admin/(announcement)/announcements/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 
+import { AnnouncementsList } from "@/components/dashboard/admin/pages/announcement/announcementList"
 import { useState, useEffect } from "react"
 
 
-export default function StudentDetailsPage() {
+export default function AdminAnnouncementList() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   useEffect(() => {
@@ -24,7 +25,7 @@ export default function StudentDetailsPage() {
 
   return (
     <div className={`transition-all ease-in-out bg-white border border-gray-200 rounded-lg p-2 duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-20'}`}>
-         AnnouceMents list
+        <AnnouncementsList />
     </div>
   )
 }
