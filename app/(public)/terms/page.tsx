1f513847b883dbb8ac88e8532f2ec947db7a4592
priest-2105 <priest2105@gmail.com>
2025-03-12 21:15:57 +0100

    update

--- app/(public)/terms/page.tsx
+++ app/(public)/terms/page.tsx
@@ -1,4 +1,3 @@
-import PrivacyPolicyPage from "@/components/public/pages/privacy-policy/prviacyDocumentation";
 import TermsAndConditionsPage from "@/components/public/pages/terms";
 
 function Terms() {
