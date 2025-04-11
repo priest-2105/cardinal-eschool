196d7a7d291357020e2b07f5cec0691226c9ff96
priest-2105 <priest2105@gmail.com>
2025-04-11 19:18:35 +0100

    commit

--- utils/pusher.ts
+++ utils/pusher.ts
@@ -3,8 +3,8 @@ import Pusher from "pusher-js"
 // Initialize Pusher with your credentials
 const pusher = new Pusher("8b36ef8aad2485f21c5f", {
   cluster: "us2",
-  // Enable encryption for secure connections
-  encrypted: true,
+  // Force TLS for secure connections
+  forceTLS: true,
 })
 
 export default pusher
