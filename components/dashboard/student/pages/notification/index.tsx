da59126005d6b1612ca4ca8fd73b09a6d6049a30
priest-2105 <priest2105@gmail.com>
2025-04-13 07:51:02 +0100

    ticket details

--- components/dashboard/student/pages/notification/index.tsx
+++ components/dashboard/student/pages/notification/index.tsx
@@ -644,7 +644,7 @@ export function NotificationList() {
         )}
 
         {/* Notifications List */}
-        <div className="p-0 max-h-[60vh] overflow-y-scroll custom-scrollbar divide-y">
+        <div className="p-0 max-h-[60vh] max-md:h-[50vh] overflow-y-scroll custom-scrollbar divide-y">
           {isLoading
             ? renderSkeleton()
             : filteredNotifications.length === 0
