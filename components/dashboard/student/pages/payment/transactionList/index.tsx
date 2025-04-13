e3bc472b8a215b85b89d0ac5629df4dd6f817536
priest-2105 <priest2105@gmail.com>
2025-04-13 07:12:53 +0100

    transaction history

--- components/dashboard/student/pages/payment/transactionList/index.tsx
+++ components/dashboard/student/pages/payment/transactionList/index.tsx
@@ -22,48 +22,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
-
-
-interface Transaction {
-  id: number
-  transaction_ref: string
-  created_at: string
-  subscription_plan_name: string
-  amount: number
-  status: string
-  quantity: number
-}
-
-interface ApiTransaction {
-  id: number;
-  transaction_ref: string;
-  created_at: string;
-  subscription_plan_name: string;
-  amount: string; 
-  status: string;
-  quantity: number;
-}
-
-const MONTHS = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-]
-
-function parseTransactionDate(dateString: string) {
-  const monthIndex = MONTHS.findIndex((month) => dateString.startsWith(month))
-  const year = Number.parseInt(dateString.match(/\d{4}/)?.[0] || "0")
-  return { month: monthIndex, year }
-}
+import { Transaction, ApiTransaction, MONTHS, parseTransactionDate } from "@/components/dashboard/student/pages/payment/utils"
 
 export default function TransactionList() {
   const router = useRouter()
